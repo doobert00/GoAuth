55c9c9ab02ba9958bef3f57bd01b1b777483edd7
doobert <dubinskyz00@gmail.com>
2024-06-06 19:13:50 -0400

    simple ui

---
+++ client/src/App.js
@@ -0,0 +1,52 @@
+import React, { useState } from 'react';
+
+const App = () => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleUsernameChange = (e) => {
+    setUsername(e.target.value);
+  };
+
+  const handlePasswordChange = (e) => {
+    setPassword(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // You can perform further validation or submit the data here
+    console.log('Username:', username);
+    console.log('Password:', password);
+    // Reset the form after submission
+    setUsername('');
+    setPassword('');
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <div>
+        <label htmlFor="username">Username:</label>
+        <input
+          type="text"
+          id="username"
+          value={username}
+          onChange={handleUsernameChange}
+          required
+        />
+      </div>
+      <div>
+        <label htmlFor="password">Password:</label>
+        <input
+          type="password"
+          id="password"
+          value={password}
+          onChange={handlePasswordChange}
+          required
+        />
+      </div>
+      <button type="submit">Submit</button>
+    </form>
+  );
+};
+
+export default App;
